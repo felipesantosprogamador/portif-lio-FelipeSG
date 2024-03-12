84145c95fe0259392b51e27f0113a5f55b7aaf2f
felipe santos <feliperose3041@gmail.com>
2024-03-11 21:47:00 -0300

    responsividade finalizada

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
-let header =document.getElementById('header')
+const menuHamburquer = document.querySelector('.menu-hamburguer');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 200)  {
-    header.style.background = '#191919'
+
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    
+    },
+ navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+    },
+});
+
+
+let haerder = ducument.getElementById('header')
+
+window.addEventListener('scroll', ()=>{
+    if (window.scrollY >= 200) {
+        header.style.background = '#191919'
     } else {
         header.style.background = 'transparent'
     }
