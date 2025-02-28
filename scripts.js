d0e08771df75a6f9fb94fbd84e2690fb1f99d2b0
felipe santos <feliperose3041@gmail.com>
2025-02-28 19:41:49 -0300

    adição de um nono arquivo

--- scripts.js
+++ scripts.js
@@ -1,28 +1,37 @@
-const menuHamburquer = document.querySelector('.menu-hamburguer');
+const menuHamburger = document.querySelector('.menu-hamburger');
+const navbarLinks = document.querySelector('.navbar-links');
 
-
-
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    
-    },
- navigation: {
-  nextEl: ".swiper-button-next",
-  prevEl: ".swiper-button-prev",
-    },
+menuHamburger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
 });
 
-
-let haerder = ducument.getElementById('header')
-
-window.addEventListener('scroll', ()=>{
-    if (window.scrollY >= 200) {
-        header.style.background = '#191919'
-    } else {
-        header.style.background = 'transparent'
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
     }
-})
+});
+
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 200) {
+        header.style.background = '#191919';
+    } else {
+        header.style.background = 'transparent';
+    }
+});
